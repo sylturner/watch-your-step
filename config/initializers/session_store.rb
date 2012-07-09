c92619b801318a1edd3df7c323ebe1949c31dda8
Syl Turner <syl.turner@hannonhill.com>
2012-07-09 09:45:59 -0400

    Changed the session store to use Ruby 1.8 style hash/key/whatever things.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-WatchYourStep::Application.config.session_store :cookie_store, key: '_watch-your-step_session'
+WatchYourStep::Application.config.session_store :cookie_store, :key => '_watch-your-step_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
