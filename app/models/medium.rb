6fa2d52894a66bef0495e9089fcff8d0f84766fb
Syl Turner <syl.turner@hannonhill.com>
2012-07-09 09:00:53 -0400

    Added capistrano to deploy, added deploy.rb to the .gitignore file. Made comments show up in created_at ascending order.

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,7 +1,7 @@
 class Medium < ActiveRecord::Base
   attr_accessible :description, :title, :year, :prompts_attributes, :comments_attributes
   has_many :prompts
-  has_many :comments
+  has_many :comments, :order => 'created_at ASC'
 
   accepts_nested_attributes_for :prompts, :allow_destroy => true
   accepts_nested_attributes_for :comments, :allow_destroy => true
